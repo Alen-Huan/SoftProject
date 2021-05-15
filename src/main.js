78674c63268fc9fe70b875d5f0f28ea9faea1809
liuhuan <liuhuan1974@outlook.com>
2021-05-15 15:40:49 +0800

    登录页面界面制作

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import "ant-design-vue/dist/antd.css";
 
-createApp(App).use(store).use(router).mount('#app');
+import { createApp } from "vue";
+
+import Antd from "ant-design-vue";
+
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+// use注册插件，注册全局组件，注册全局指令
+createApp(App)
+.use(store)
+.use(router)
+.use(Antd)
+.mount('#app');
---
+++ src/views/Auth/index.js
@@ -0,0 +1,18 @@
+import { defineComponent } from "vue";
+
+import {
+  LockOutlined,
+  MailOutlined,
+  UserOutlined
+} from "@ant-design/icons-vue";
+
+export default defineComponent({
+    components: {
+        UserOutlined,
+        LockOutlined,
+        MailOutlined
+    },
+    setup(){
+        
+    }
+})
